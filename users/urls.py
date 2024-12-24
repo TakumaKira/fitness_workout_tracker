e8caa669e940e90ea33c740ed46a9a7cabdf4888
Takuma Kira <takuma@kirakiraworx.com>
2024-12-24 17:47:51 +0900

    [dev]: Add ability to create new user

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.register, name='register'),
 ] 
